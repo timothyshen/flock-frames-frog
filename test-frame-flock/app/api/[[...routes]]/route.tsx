3074698af6c594ef50d1b8893fb3eaf5d5e46d14
timothyshen <timothyshen1@hotmail.com>
2024-03-05 23:58:39 +0000

    first commit

---
+++ test-frame-flock/app/api/[[...routes]]/route.tsx
@@ -0,0 +1,64 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { handle } from 'frog/next'
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub API URL to enable frame verification.
+  // hubApiUrl: 'https://api.hub.wevm.dev',
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+            : 'Welcome!'}
+        </div>
+      </div>
+    ),
+    intents: [
+      // <TextInput placeholder="Enter custom fruit..." />,
+      <Button value="Lets Start">Lets Start</Button>,
+    ],
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
