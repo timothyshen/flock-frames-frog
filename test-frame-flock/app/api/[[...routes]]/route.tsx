4cbf7a2b3018b9eb4e11f508602ebba0b571cbdb
timothyshen <timothyshen1@hotmail.com>
2024-03-06 23:31:16 +0000

    adding svg

--- test-frame-flock/app/api/[[...routes]]/route.tsx
+++ test-frame-flock/app/api/[[...routes]]/route.tsx
@@ -2,6 +2,7 @@
 
 import { Button, Frog, TextInput } from 'frog'
 import { handle } from 'frog/next'
+import FLockLogo from '@/public/FLockLogo.svg'
 
 const app = new Frog({
   assetsPath: '/',
@@ -35,6 +36,13 @@ app.frame('/', (c) => {
           width: '100%',
         }}
       >
+        <FLockLogo
+          style={{
+            height: 200,
+            width: 200,
+          }}
+        />
+
         <div
           style={{
             color: 'white',
@@ -47,18 +55,99 @@ app.frame('/', (c) => {
             whiteSpace: 'pre-wrap',
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          FLock.io RAG chat playground
+        </div>
+        <div
+          style={{
+            color: 'white',
+            fontSize: 48,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Lets chat about Ecosystem!
         </div>
       </div>
     ),
     intents: [
-      // <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="Lets Start">Lets Start</Button>,
+      <Button action='/chat' value="Lets Start">Lets Start</Button>,
+      <Button.Link href="https://flock.io">FLock</Button.Link>,
     ],
   })
 })
+
+app.frame('/chat', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    action: '/chat',
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <FLockLogo
+          style={{
+            height: 200,
+            width: 200,
+          }}
+        />
+
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          FLock.io RAG chat playground
+        </div>
+        <div
+          style={{
+            color: 'white',
+            fontSize: 48,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Lets chat about Ecosystem!
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Enter your fruit..." />,
+      <Button action='/chat' value="Send"> Send </Button>,
+      <Button action='/' value="Go Back"> Go Back </Button>,
+    ],
+  })
+})
+
 
 export const GET = handle(app)
 export const POST = handle(app)
