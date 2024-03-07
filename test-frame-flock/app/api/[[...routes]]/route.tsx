a854a75e05fc2c96c542c4437d44927c45265dfd
timothyshen <timothyshen1@hotmail.com>
2024-03-07 00:16:13 +0000

    adding third frame

--- test-frame-flock/app/api/[[...routes]]/route.tsx
+++ test-frame-flock/app/api/[[...routes]]/route.tsx
@@ -141,13 +141,66 @@ app.frame('/chat', (c) => {
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter your fruit..." />,
-      <Button action='/chat' value="Send"> Send </Button>,
+      <TextInput placeholder="Enter your question..." />,
+      <Button action='/submit' value="Send"> Send </Button>,
       <Button action='/' value="Go Back"> Go Back </Button>,
     ],
   })
 })
 
+
+app.frame('/submit', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <FLockLogo
+          style={{
+            height: 200,
+            width: 200,
+          }}
+        />
+
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            && `${fruit}`}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button action='/' value="Go Back"> Go Back </Button>,
+    ],
+  })
+})
 
 export const GET = handle(app)
 export const POST = handle(app)
